c07cc1ec0bb68932bf1cc437f9f696b033673772
Binary-Blade <codegrunt@proton.me>
2024-10-11 21:30:33 +0200

    * refactor: Restructure code and create two sub-function for a better visibility

--- evaluated_command.py
+++ evaluated_command.py
@@ -4,6 +4,18 @@ from openai_helpers import analyze_code
 from score_manager import score_count, has_been_evaluated, mark_as_evaluated
 
 
+def get_analyzed_code(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        code = file.read()
+    return analyze_code(code)
+
+
+def generate_new_subject(file_path: str) -> str:
+    subject, extension = os.path.splitext(file_path)
+    lang = extension[1:]  # recup extension of the precedent exercice
+    create_file(lang, subject)
+
+
 def evaluate_command(file_path: str, global_score: dict) -> str:
     try:
         if has_been_evaluated(file_path):
@@ -11,19 +23,15 @@ def evaluate_command(file_path: str, global_score: dict) -> str:
                   file_path}' has already been evaluated. You cannot evaluate it again.")
             return
 
-        with open(file_path, 'r') as file:
-            code = file.read()
-        result = analyze_code(code)
-
+        # Analyze file code provided
+        result = get_analyzed_code(file_path)
         # Calculate the score with score_count function
         score_count(result, global_score)
         mark_as_evaluated(file_path)
 
         # HACK: Dont work
         # generare new subject
-        subject, extension = os.path.splitext(file_path)
-        lang = extension[1:]  # recup extension of the precedent exercice
-        create_file(lang, subject)
+        generate_new_subject(file_path)
 
     except FileNotFoundError:
         print(f"Error: The file '{
