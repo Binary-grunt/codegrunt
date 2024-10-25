d9ebc5fb0d2be3bf9e0bd60a597c1bc4a625bbaa
Binary-Blade <codegrunt@proton.me>
2024-10-25 13:48:27 +0200

    * feat: Create new file for globals var

---
+++ config.py
@@ -0,0 +1,27 @@
+# Languages options
+LANGUAGES = {
+    '1': 'PHP',
+    '2': 'JavaScript',
+    '3': 'Python',
+}
+
+
+# Global dictionary for file extensions and language names
+LANGUAGE_EXTENSIONS = {
+    "javascript": ("js", "JavaScript"),
+    "python": ("py", "Python"),
+    "php": ("php", "PHP"),
+}
+
+# Path for the current exercise file
+CURRENT_EXERCISE_FILE_PATH = None
+
+
+# Score settings
+GLOBAL_SCORE = {
+    "total_score": 0,
+    "exercises_completed": 0
+}
+
+#  Path for the file that contains the evaluated exercises
+EVALUATED_FILES = "evaluated_exercises.txt"
--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-from generate_command import generate_command
-from evaluated_command import evaluate_command
+from commands.generate_command import generate_command
+from commands.submit_command import submit_command
+from config import GLOBAL_SCORE
 
 # TODO: Add system of logging to log the result and explaination
 # for each exercice in .txt or .md local link to the folder,
 # push after each evaluate command
-global_score = {
-    "total_score": 0,
-    "exercises_completed": 0
-}
 
 
 def main():
     print("Hello, Welcome to Codegrunt. It's generator of exercice.")
 
-    while global_score["exercises_completed"] < 10:
+    while GLOBAL_SCORE["exercises_completed"] < 10:
 
         print(" _____________________ \n"
               "\n"
-              "To continue, you have 2 commands available \n"
+              "To continue, you have 1 command available \n"
               "generate - For generate exercice\n"
-              "evaluate - For correct the provided path exercice\n"
               )
         choice_command = input("Choice a command : ")
 
         if choice_command == "generate":
             generate_command()
-        elif choice_command == "evaluate":
+        elif choice_command == "submit":
             # Evaluate the files
             analyze = input("Write the name of the file to evaluate : ")
-            evaluate_command(analyze, global_score)
+            submit_command(analyze, GLOBAL_SCORE)
 
 
 if __name__ == '__main__':
