0c28744ec6b7a528836536fba8b7f75fef3e5f42
Binary-grunt <CodeGrunt@proton.me>
2024-12-28 07:56:39 +0100

    * chore(main): Add comments

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+import typer
 from database import initialization_db
 from database.sqlite_config import SessionLocal
-import typer
 from api import OpenAIService
-from core.interface_cli import ApplicationInterface, PreferenceCli
+from core.interface_cli import CliController, PreferenceCli
 from core.session_manager import SessionManager
 from core.managers import UserManager, ExerciseManager
 
 
-def main():
+def main() -> None:
+    """
+    Entry point for the CodeGrunt CLI application.
+
+    This function initializes the database, sets up necessary managers,
+    and then launches the main CLI controller. It uses Typer for command-line
+    argument parsing and execution.
+    """
     initialization_db()
     db = SessionLocal()
+
     openai_service = OpenAIService()
     exercise_manager = ExerciseManager(openai_service)
     session_manager = SessionManager(db, exercise_manager)
-
-    # Initialize components
     user_manager = UserManager(session_manager)
     preference_manager = PreferenceCli()
-    app_interface = ApplicationInterface(session_manager, user_manager, preference_manager)
 
-    # Run the application
+    app_interface = CliController(session_manager, user_manager, preference_manager)
+
     app_interface.run()
 
 
