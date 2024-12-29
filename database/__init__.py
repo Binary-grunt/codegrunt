fb35c39e9e34d5096cb8133f9b3445e4215474b1
Binary-grunt <CodeGrunt@proton.me>
2024-12-29 19:50:43 +0100

    * refactor: Add wait_for_db inside the init

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,5 @@
 from .sqlite_config import SessionLocal, engine
 from .initialization_db import initialization_db
+from .wait_for_db import wait_for_db
 
-__all__ = ["SessionLocal", "engine", "initialization_db"]
+__all__ = ["SessionLocal", "engine", "initialization_db", "wait_for_db"]
